c7bd7c2b576ae0921eecf863dad46d12c777ce96
liloman <eselilo@gmail.com>
2016-02-23 23:23:39 +0100

    *client done

--- pomodoro-client.sh
+++ pomodoro-client.sh
@@ -2,8 +2,10 @@
 #Simple client for pomodoro-daemon.sh
 readonly API=/dev/shm/pomodoro
 readonly LOCK=/dev/shm/pomodoro.lock
+readonly FIFO=/dev/shm/pomodoro.app
 
 call() {
+
     while true;do
         {
             flock -w 5 -x 7 || { echo "Couldn't acquire the lock" >&2; continue; }
@@ -11,17 +13,23 @@ call() {
             break
         } 7>$LOCK
     done
+    #wait for response
     sleep 0.1
     echo $(<$API)
 }
 
+usage() { echo "Unknown option: try start,pause,stop,status or quit to close the daemon"; }
 
-date
+com() {
+    [[ ! -p $FIFO ]] && { echo "Daemon not running"; return; }
+    call $1
+    call status
+}
 
-call start
-#call pause
-#call stop
-#call quit
-# call status
+case $1 in
+    start|pause|stop|status|quit)  com $1
+        ;;
+    *                           ) usage
+        ;;
+esac
 
-date
