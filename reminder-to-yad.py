7d5d9859a9ee5f10f9a7098368d59fd18fe1ffe9
liloman <eselilo@gmail.com>
2016-12-20 13:06:59 +0100

    Dont trace on empty json

--- reminder-to-yad.py
+++ reminder-to-yad.py
@@ -19,9 +19,13 @@ command = "task +reminder +PENDING rc.verbose=nothing rc.json.array=no export"
 DATEFORMAT = '%Y%m%dT%H%M%SZ'
 
 for task in commands.getoutput(command).split("\n"):
-    data = json.loads(task)
-    date=datetime.strptime(data['due'], DATEFORMAT)
-    print u"{0}\n{1}\n{2}\n{3}".format(data['id'],data['description'],"{0}/{1}/{2}".format(date.day,date.month,date.year),"FALSE").encode('utf-8').strip()
+    try:
+        data = json.loads(task)
+        date=datetime.strptime(data['due'], DATEFORMAT)
+        print u"{0}\n{1}\n{2}\n{3}".format(data['id'],data['description'],"{0}/{1}/{2}".format(date.day,date.month,date.year),"FALSE").encode('utf-8').strip()
+    except:
+        sys.exit (0)
+
 
 sys.exit (0)
 
